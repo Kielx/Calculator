a5ee4ca8df7c74b82905ef758b0bcadd53318ef1
Kielx <Kiel66@gmail.com>
2020-02-10 12:17:42 +0100

    working 0.1 version

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let operations = {
     add : function(a,b){
         return a + b;
+        
+        
     },
 
     subtract : function(a,b){
@@ -18,26 +20,30 @@ let operations = {
 
 const addToNum = function(text){
     if (active){
+        calc.disabled = false;
         num2+= text;
     }
     else{
         num1 += text ;
+        calc.disabled = true;
     }
     
     console.log(`num1 ${num1}`)
     console.log(`num2 ${num2}`)
+    console.log(`operator ${operator}`)
 }
-
-
-let result;
-let tempres;
-let num1 = '';
-let operator = '';
-let num2 = '';
-let func;
 
 num = document.querySelectorAll('.num');
 op = document.querySelectorAll('.op');
+calc = document.querySelector('.calc')
+clr = document.querySelector('.clr')
+
+calc.disabled = true;
+
+let result;
+let num1 = '';
+let operator = '';
+let num2 = '';
 let active = false;
 
 let operators = new Map();
@@ -55,19 +61,39 @@ op.forEach(element => {
         element.addEventListener("click", () => {
             active = true;
             operator = element.textContent
+            if(num1 && num2){
+                calc.disabled = false;
+            }
+            
         }
         )
     }
 )
 
-calc = document.querySelector('.calc')
 calc.addEventListener('click', () => {
-    dupa = operators.get(operator)
-    result = dupa(num1, num2)
+    operator = operators.get(operator)
+    result = operator(+num1, +num2)
     console.log(result)
+    num1 = result
+    num2 = ''
+    calc.disabled = true;
+    
+    
 }
 )
 
+clr.addEventListener('click', () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    result = '';
+    active = false;
+    console.log(num1)
+    console.log(num2)
+    console.log(operator)
+    
+}
+)
 
 
 
